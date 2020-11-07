99afa29d4ae275b2ecfa4ec490f176e358ee8e07
Zeyneb Esendir <zeynebesendir@gmail.com>
2020-11-07 23:31:49 +0000

    Updates

---
+++ precache-manifest.ee87867760744c693abad5e11effa7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb830891726720a9644810861e454591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2eb0a5e99ee178ba8b7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "901401cbcbf50c5514a4",
+    "url": "/static/js/2.b2984663.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.b2984663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2eb0a5e99ee178ba8b7",
+    "url": "/static/js/main.04128e70.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
